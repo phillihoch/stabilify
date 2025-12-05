46454b058d8550cb93164ba435e80d7e7c14d4c5
Philipp Hoch <mail@philipp-hoch.de>
2025-12-05 08:14:42 +0100

    reset develpment

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { AIClient } from "./core/ai/client";
-export type { SelectorImprovementResult } from "./core/ai/client";
-export { extendPage } from "./core/page-extension/extend-page";
-export type { ExtendedPage, HealingResult, StabilifyOptions } from "./types";
